a3e38807ad9d2645923e1b147c7e7d9f1cd0462d
Hemant391 <148564538+Hemant391@users.noreply.github.com>
2024-06-30 09:08:49 +0530

    strach

---
+++ src/App.js
@@ -0,0 +1,54 @@
+
+import './App.css';
+import React, { Component, useEffect, useState } from 'react'
+
+
+function App() {
+  const [greet,SetGreet]=useState(false);
+  const [warn,setWarn]=useState(false)
+  const [data,setData]=useState({
+    'Username':'',
+    'Password':''
+  })
+useEffect(()=>{
+localStorage.setItem('crio','password')
+
+},[])
+
+const handlesubmit=(event)=>{
+  event.preventDefault();
+console.log(event.target);
+if(localStorage.getItem(data.Username)===data.Password){
+  SetGreet(true);
+  setWarn(false)
+}else{
+  setWarn(true)
+}
+
+}
+const handleinput=(e)=>{
+  setData(prev=>({...prev,[e.target.name]:e.target.value}))
+}
+
+  return (
+    <div className="App">
+     <h1>Login Page</h1>
+     {warn?<p>Invalid username or password</p>:''}
+     {!greet? <form  onSubmit={handlesubmit}>
+      <p>
+      <label >Username:</label>
+      <input name='Username' type="text" value={data.Username} onChange={handleinput} required />
+      </p>
+      <p>
+      <label >Password:</label>
+      <input name='Password' type="text" value={data.Password} onChange={handleinput} required />
+
+      </p>
+      <button type="submit" >Submit</button>
+     </form> : <p>Welcome,user!</p>}
+     
+    </div>
+  );
+}
+
+export default App;
